3f62da8dacf53019e996a02035d8dedbae7e3330
Jonathan Ray <jonathanray17@gmail.com>
2019-05-02 13:58:08 -0400

    adds simple backend for get/post requests

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
 
+// when get request comes in, sent back all the messages
+const db = [
+    'Welcome to the ChatApp',
+    '🐈',
+    'Okay.'
+];
+app.get('/', (req, res) => {
+    res.json(db);
+});
+
+// when post request comes in, add message to arry of messages
 
 app.listen(3000, ()=> {console.log("Running on port 3000");});
